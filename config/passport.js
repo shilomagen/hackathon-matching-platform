26f8994c3904b7ec6159480d91c7c938dc66378c
Shilo Mangam <shilo.mangam@sap.com>
2017-05-11 19:28:51 +0300

    ES6 little change

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,16 @@
-var passport = require('passport'),
+const passport = require('passport'),
     mongoose = require('mongoose');
 
-module.exports = function () {
-    var User = mongoose.model('User');
-
-    passport.serializeUser(function (user, done) {
+module.exports = function() {
+    const User = mongoose.model('User');
+    passport.serializeUser((user, done) => {
         done(null, user.id);
     });
-
-    passport.deserializeUser(function (id, done) {
-        User.findOne(
-            {_id: id},
-            '-password',
-            function (err, user) {
-                done(err, user);
-            }
-        );
+    passport.deserializeUser((id, done) => {
+        User.findOne({_id: id}, '-password', (err, user) => {
+            done(err, user);
+        });
     });
 
-    require('./strategies/local.js')();
+    // require('./strategies/local.js')();
 };
--- config/strategies/local.js
+++ config/strategies/local.js
@@ -1,16 +1,14 @@
-var passport = require('passport'),
+const passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     User = require('mongoose').model('User');
 
-module.exports = function () {
-    passport.use(new LocalStrategy(function (email, password, done) {
-        User.findOne(
-            {email: {$regex: new RegExp(email, "i")}},
-            function (err, user) {
+module.exports = function() {
+    passport.use(new LocalStrategy((email, password, done) => {
+        User.findOne({email: {$regex: new RegExp(email, "i")}},
+            (err, user) => {
                 if (err) {
                     return done(err);
                 }
-
                 if (!user) {
                     return done(null, false, {message: 'Unknown user'});
                 }
